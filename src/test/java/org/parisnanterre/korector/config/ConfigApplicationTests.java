ff1ddcafa49f185f3c68391cf7e565be06a2c2d5
zakari KARAHACANE <zkarahacane@acton.local>
2020-04-10 14:01:41 +0200

    commit creation

---
+++ src/test/java/org/parisnanterre/korector/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package org.parisnanterre.korector.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
